9e9e7fe63143d0f6a7747e20de4871072e74ef3b
Alexa <amzeazas@comcast.net>
2015-09-25 09:01:23 -0700

    Update firebase URL.

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'question-answers',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
+    firebase: 'https://question-answers.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
