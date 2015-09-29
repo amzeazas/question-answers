af6d9d579140bbfeba87030b3712c7fdf83f3e1b
Alexa <amzeazas@comcast.net>
2015-09-28 23:40:10 -0700

    Move update/delete question buttons to question route.

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  updateQuestion: false,
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
@@ -32,6 +33,24 @@ export default Ember.Route.extend({
         question.save();
       });
       this.transitionTo('question', question.question_id);
+    },
+
+    update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key] !== undefined) {
+          question.set(key, params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    },
+
+    destroyQuestion(question) {
+      question.get('answers').forEach(function(answer) {
+        answer.destroyRecord();
+      });
+      question.destroyRecord();
+      this.transitionTo('index');
     }
   }
 });
